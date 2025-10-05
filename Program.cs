5665cd14a499ec437eb50ea1b1cb7f0e6807bc76
Stas <zyxstas@gmail.com>
2025-10-05 10:53:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace asynchron
+{
+    internal class Program
+    {
+        static string ProcessData(string dataName)
+        {
+            Thread.Sleep(3000);
+            return $"Обработка '{dataName}' завершена за 3 секунды";
+        }
+
+        static void Main(string[] args)
+        {       
+            Console.WriteLine(ProcessData("файл1"));
+            Console.WriteLine(ProcessData("файл2"));
+            Console.WriteLine(ProcessData("файл3"));
+        }
+    }
+}
+
