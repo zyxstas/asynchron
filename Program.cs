92af420a5f8ef3ed31c24e53ca94706db5ac24f0
Stas <zyxstas@gmail.com>
2025-10-05 11:38:03 +0300

    добавлен  асинхронный метод

--- Program.cs
+++ Program.cs
@@ -4,15 +4,35 @@
     {
         static string ProcessData(string dataName)
         {
+            Console.WriteLine($"Синхронная обработка {dataName}");
             Thread.Sleep(3000);
             return $"Обработка '{dataName}' завершена за 3 секунды";
         }
+        static async Task<string> ProcessDataAsync(string dataName)
+        {
+            Console.WriteLine($"Асинхронная обработка {dataName}");
+            await Task.Delay(3000);
+            return $"Обработка '{dataName}' завершена за 3 секунды";
+        }
 
-        static void Main(string[] args)
-        {       
-            Console.WriteLine(ProcessData("файл1"));
-            Console.WriteLine(ProcessData("файл2"));
-            Console.WriteLine(ProcessData("файл3"));
+        static async Task Main(string[] args)
+        {            
+            Console.WriteLine(ProcessData("Файл 1"));
+            Console.WriteLine(ProcessData("Файл 2"));
+            Console.WriteLine(ProcessData("Файл 3"));
+
+            var task1 = ProcessDataAsync("Файл 1");
+            var task2 = ProcessDataAsync("Файл 2");
+            var task3 = ProcessDataAsync("Файл 3");
+
+            // Ждем завершения всех задач
+            var results = await Task.WhenAll(task1, task2, task3);
+
+            // Выводим результаты
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
         }
     }
 }
