21ec397a07a5344e903159044528aacbb6d45ce8
Stas <zyxstas@gmail.com>
2025-10-05 11:48:08 +0300

    добавлен stopwatch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace asynchron
+﻿using System.Diagnostics;
+
+namespace asynchron
 {
     internal class Program
     {
@@ -16,23 +18,29 @@
         }
 
         static async Task Main(string[] args)
-        {            
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             Console.WriteLine(ProcessData("Файл 1"));
             Console.WriteLine(ProcessData("Файл 2"));
             Console.WriteLine(ProcessData("Файл 3"));
+            stopwatch.Stop();
+            Console.WriteLine($"Cинхронная обработка выполнена за: {stopwatch.Elapsed.TotalSeconds} секунд\n");
 
+            stopwatch.Restart();
             var task1 = ProcessDataAsync("Файл 1");
             var task2 = ProcessDataAsync("Файл 2");
             var task3 = ProcessDataAsync("Файл 3");
-
-            // Ждем завершения всех задач
+                        
             var results = await Task.WhenAll(task1, task2, task3);
 
-            // Выводим результаты
             foreach (var result in results)
             {
                 Console.WriteLine(result);
             }
+            stopwatch.Stop();
+            Console.WriteLine($"Асинхронная обработка выполнена за: {stopwatch.Elapsed.TotalSeconds} секунд\n");
+
         }
     }
 }
